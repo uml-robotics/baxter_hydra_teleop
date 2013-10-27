03a2d670b89461035d048f93eb790cee00954326
Mikhail Medvedev <mmedvede@cs.uml.edu>
2013-10-27 19:54:31 -0400

    Use stl mesh for hydra marker. Adds the mesh

--- src/vis/vis.py
+++ src/vis/vis.py
@@ -39,17 +39,23 @@ class Vis(object):
         msg.pose.position.y = +0.025
         self.pub.publish(msg)
 
-        # Hydra
+        msg.type = Marker.MESH_RESOURCE
         msg.id += 1
-        msg.scale.x = 0.06
-        msg.scale.y = 0.04
-        msg.scale.z = 0.18
-        msg.pose.position.x = -0.09
+        msg.scale.x = 1
+        msg.scale.y = 1
+        msg.scale.z = 1
+        msg.pose.position.x = -0.04
         msg.pose.position.y = 0
-        msg.pose.position.z = 0
-        msg.pose.orientation.z = 1
+        msg.pose.position.z = -0.01
+
+        msg.lifetime = rospy.Duration()
+        msg.pose.orientation.x = 0
+        msg.pose.orientation.y = 0
+        msg.pose.orientation.z = 0
+        msg.pose.orientation.w = 1
         msg.color.r = 0.2
         msg.color.g = 0.2
         msg.color.b = 0.2
-        msg.color.a = 0.2
+        msg.color.a = 0.5
+        msg.mesh_resource = 'package://baxter_hydra_teleop/meshes/hydra.stl'
         self.pub.publish(msg)
